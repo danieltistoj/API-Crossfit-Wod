1f87f00a2c6139a8f806d4e5aa55c11c6e72d36a
danieltistoj <jose.tisrey@gmail.com>
2022-08-01 22:22:38 -0600

    api crossfit

---
+++ src/services/workoutService.js
@@ -0,0 +1,19 @@
+const getAllWorkouts = ()=>{
+    return;
+}
+
+const getOneWorkout= ()=>{
+    return;
+}
+
+const createNeWorkou = ()=>{
+    return;
+}
+
+const updateOneWorko = ()=>{
+    return;
+}
+
+const deleteOneWorko = ()=>{
+    return;
+}
