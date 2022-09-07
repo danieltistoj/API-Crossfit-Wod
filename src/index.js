78d74785d0b224b57df86498e4064c5336c0bdb6
danieltistoj <jose.tisrey@gmail.com>
2022-09-07 03:08:04 -0600

    hola mundo en despliegue

--- src/index.js
+++ src/index.js
@@ -5,5 +5,8 @@ const app = express()
 const PORT = process.env.PORT || 3000
 app.use(express.json())
 app.use("/api/v1/workouts",v1WorkoutRouter)
+app.get('/', function (req, res) {
+    res.send('Hello World')
+    })
 
 app.listen(PORT,()=>{console.log(`Servidor en el puerto ${PORT}`)})
